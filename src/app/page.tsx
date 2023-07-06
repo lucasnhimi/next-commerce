a9ec6949fcf968875000aa683981dd4f064d1dba
Lucas Nhimi <lucasnhimi@gmail.com>
2023-07-05 21:58:04 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-commerce 13</h1>
+    <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
+      <div
+        className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6
+      '
+      >
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 1
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 1
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 1
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 1
+        </div>
+      </div>
     </div>
   );
 }
