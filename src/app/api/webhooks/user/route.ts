608b89991b96d6e079255623d689bc58be602642
Lucas Nhimi <lucasnhimi@gmail.com>
2023-07-08 12:53:26 -0300

    Adicionando endpoint para o webhook do Clerk

---
+++ src/app/api/webhooks/user/route.ts
@@ -0,0 +1,80 @@
+import prisma from '@/lib/prisma';
+import { IncomingHttpHeaders } from 'http';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { Webhook, WebhookRequiredHeaders } from 'svix';
+
+const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
+
+type EventType = 'user.created' | 'user.updated' | '*';
+
+type Event = {
+  data: EventDataType;
+  object: 'event';
+  type: EventType;
+};
+
+type EventDataType = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email_addresses: EmailAddressType[];
+  primary_email_address_id: string;
+  attributes: Record<string, string | number>;
+};
+
+type EmailAddressType = {
+  id: string;
+  email_address: string;
+};
+
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    'svix-id': headersList.get('svix-id'),
+    'svix-timestamp': headersList.get('svix-timestamp'),
+    'svix-signature': headersList.get('svix-signature'),
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+
+  try {
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as Event;
+  } catch (err) {
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
+
+  const eventType: EventType = evt.type;
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      primary_email_address_id,
+      ...attributes
+    } = evt.data;
+
+    await prisma.user.upsert({
+      where: { externalId: id as string },
+      create: {
+        externalId: id as string,
+        attributes,
+      },
+      update: {
+        attributes,
+      },
+    });
+  }
+
+  return NextResponse.json({}, { status: 200 });
+}
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
