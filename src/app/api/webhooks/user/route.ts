153d710acb1ee7d498f94637af835eb68a5f635f
Lucas Nhimi <lucasnhimi@gmail.com>
2023-07-09 18:20:09 -0300

    Adicionando usuario no Stripe

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -3,6 +3,7 @@ import { IncomingHttpHeaders } from 'http';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { Webhook, WebhookRequiredHeaders } from 'svix';
+import Stripe from 'stripe';
 
 const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
 
@@ -60,10 +61,21 @@ async function handler(request: Request) {
       ...attributes
     } = evt.data;
 
+    // inserir usuario no stripe
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+      apiVersion: '2022-11-15',
+    });
+
+    const customer = await stripe.customers.create({
+      name: `${first_name} ${last_name}`,
+      email: email_addresses ? email_addresses[0].email_address : '',
+    });
+
     await prisma.user.upsert({
       where: { externalId: id as string },
       create: {
         externalId: id as string,
+        stripeCustomerId: customer.id,
         attributes,
       },
       update: {
