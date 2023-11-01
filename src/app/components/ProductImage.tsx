db0edf56aa53bcdbf725de184661c23c00774a03
Lucas Nhimi <lucasnhimi@gmail.com>
2023-10-31 23:04:39 -0300

    Fixing general build errors

--- src/app/components/ProductImage.tsx
+++ src/app/components/ProductImage.tsx
@@ -14,7 +14,7 @@ export default function ProductImage({ product, fill }: ProductImageProps) {
     <Image
       src={product.image}
       fill
-      alt={product.title}
+      alt={product.name}
       className={`object-cover ${
         loading
           ? 'scale-110 blur-3xl grayscale'
@@ -27,7 +27,7 @@ export default function ProductImage({ product, fill }: ProductImageProps) {
       src={product.image}
       width={400}
       height={700}
-      alt={product.title}
+      alt={product.name}
       className={`object-cover ${
         loading
           ? 'scale-110 blur-3xl grayscale'
